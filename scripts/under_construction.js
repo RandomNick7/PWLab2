f0542c401ee8606f8803bc26febb11e52f63ac50
RandomNick7 <dorin.otgon@gmail.com>
2024-02-25 16:37:24 +0200

    Improved the placeholder page, fixed a ticker bug

--- scripts/under_construction.js
+++ scripts/under_construction.js
@@ -2,16 +2,11 @@ function extendStrips(){
     strip = document.getElementsByClassName("ticker-body");
     // For both strips, copy their contents until reaching their width
     Array.from(strip).forEach(elem => {
-        let instanceWidth = Array.from(elem.children).reduce(
-            (total, i) => total + i.offsetWidth, 0
-        )
-
-        let repeatCount = Math.ceil(elem.offsetWidth / instanceWidth)
-        let repeatText = elem.innerHTML;
-        elem.innerHTML = repeatText.repeat(repeatCount);
+        let stripUnit = elem.children[0];
+        let repeatCount = Math.ceil(elem.offsetWidth / stripUnit.offsetWidth)+1;
+        elem.innerHTML = stripUnit.outerHTML.repeat(repeatCount);
     })
 }
-
 
 window.onload = extendStrips;
 window.onresize = extendStrips;
